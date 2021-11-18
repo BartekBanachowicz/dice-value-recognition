c904ca9849be48ce8fbf380251b057f90410fe7e
Jorewin <blazejowski.j@gmail.com>
2021-11-18 19:56:27 +0100

    Set up the application

---
+++ dvr/__init__.py
@@ -0,0 +1,5 @@
+import os.path
+
+__version__ = "0.0.1"
+__authors__ = ["Bartek Banachowicz", "Julia Bielecka", "Jakub Błażejowski"]
+__root__ = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
