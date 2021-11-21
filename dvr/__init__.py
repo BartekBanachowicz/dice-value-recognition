9a2bf368e3940fd6958e5c311850b7c1d537f574
Jorewin <blazejowski.j@gmail.com>
2021-11-21 20:51:45 +0100

    Simplify module's interface

--- dvr/__init__.py
+++ dvr/__init__.py
@@ -1,4 +1,4 @@
-from . import methods
+from .methods import segmentation
 
 __version__ = "0.1.1"
 __authors__ = ["Bartek Banachowicz", "Julia Bielecka", "Jakub Błażejowski"]
--- dvr/scripts/detect.py
+++ dvr/scripts/detect.py
@@ -48,7 +48,7 @@ def main(interactive: bool, output: str, mode: str, input_: str):
 
     for i, file in enumerate(files, start=1):
         ax = fig.add_subplot(h_size, w_size, i)
-        dvr.methods.segmentation(file, ax) if mode == "segmentation" else None
+        dvr.segmentation(file, ax) if mode == "segmentation" else None
         ax.set_axis_off()
 
     fig.savefig(output)
